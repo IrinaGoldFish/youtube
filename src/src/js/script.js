5d791eb58ffbeb9df3a88b334104106b23ed3460
GoldFish <panchenko191280@gmail.com>
2019-04-10 16:45:22 +0300

    исправления

--- src/src/js/script.js
+++ src/src/js/script.js
@@ -1,34 +1,36 @@
-const  switcher = document.querySelector('#cbx'),
-       more =  document.querySelector('.more'),
-       modal = document.querySelector('.modal'),
-       videos = document.querySelectorAll('.videos__item');
-let player;       
-function bindSlideToggle(trigger,boxBody, content, openClass ){
+const switcher = document.querySelector('#cbx'),
+      more = document.querySelector('.more'),
+      modal = document.querySelector('.modal'),
+      videos = document.querySelectorAll('.videos__item');
+let player;
+
+function bindSlideToggle(trigger, boxBody, content, openClass) {
     let button = {
-        'element' : document.querySelector(trigger),
-        'active' : false
-};
+        'element': document.querySelector(trigger),
+        'active': false
+    };
     const box = document.querySelector(boxBody),
-          boxContent =  document.querySelector(content);
+          boxContent = document.querySelector(content);
+
     button.element.addEventListener('click', () => {
         if (button.active === false) {
             button.active = true;
             box.style.height = boxContent.clientHeight + 'px';
             box.classList.add(openClass);
-
-        } else {
+        }else{
             button.active = false;
             box.style.height = 0 + 'px';
             box.classList.remove(openClass);
         }
-    });      
+    });
 }
-bindSlideToggle('.hamburger','[data-slide="nav"]', '.header__menu', 'slide-active');
+
+bindSlideToggle('.hamburger', '[data-slide="nav"]', '.header__menu', 'slide-active');
 
 function switchMode() {
-    if (night === false) {
-        //document.body.style.backgroundColor = '#000';
+    if (night === false){
         night = true;
+        //document.body.style.backgroundColor = '#000';
         document.body.classList.add('night');
         document.querySelectorAll('.hamburger > line').forEach(item => {
             item.style.stroke = '#fff';
@@ -39,10 +41,10 @@ function switchMode() {
         document.querySelectorAll('.videos__item-views').forEach(item => {
             item.style.color = '#fff';
         });
+
         document.querySelector('.header__item-descr').style.color = '#fff';
         document.querySelector('.logo > img').src = 'logo/youtube_night.svg';
-
-    } else {
+    }else {
         night = false;
         document.body.classList.remove('night');
         document.querySelectorAll('.hamburger > line').forEach(item => {
@@ -56,135 +58,126 @@ function switchMode() {
         });
         document.querySelector('.header__item-descr').style.color = '#000';
         document.querySelector('.logo > img').src = 'logo/youtube.svg';
+
     }
 }
-
-
-
-
-
 
 let night = false;
 switcher.addEventListener('change', () => {
     switchMode();
-});
+})
+
 
 const data = [
     ['img/thumb_3.webp', 'img/thumb_4.webp', 'img/thumb_5.webp'],
     ['#3 Верстка на flexbox CSS | Блок преимущества и галерея | Марафон верстки | Артем Исламов',
-        '#2 Установка spikmi и работа с ветками на Github | Марафон вёрстки  Урок 2',
-        '#1 Верстка реального заказа landing Page | Марафон вёрстки | Артём Исламов'],
+       '#2 Установка spikmi и работа с ветками на Github | Марафон вёрстки Урок 2',
+       '#1 Верстка реального заказа landing page | Марафон верстки | Артем Исламов'],
     ['3,6 тыс. просмотров', '4,2 тыс. просмотров', '28 тыс. просмотров'],
     ['X9SmcY3lM-U', '7BvHoh0BrMw', 'mC8JW_aG2EM']
-];
+  ];
 
-more.addEventListener("click", () => {
-    const videosWrapper = document.querySelector(".videos__wrapper");
+more.addEventListener('click', () => {
+    const videosWrapper = document.querySelector('.videos__wrapper');
     more.remove();
 
     for (let i = 0; i < data[0].length; i++) {
-        let card = document.createElement("a");
-
-        card.classList.add("videos__item", "videos__item-active");
-        card.setAttribute("data-url", data[3][i]);
-
+        let card = document.createElement('a');
+        card.classList.add('videos__item', 'videos__item-active');
+        card.setAttribute('data-url', data[3][i]);
         card.innerHTML = `
-            <img src="${data[0][i]}" alt="thumb">
-            <div class="videos__item-descr">
-                ${data[1][i]}
-            </div>
-            <div class="videos__item-views">
-                ${data[2][i]}
-            </div>
+        <img src="${data[0][i]}" alt="thumb">
+        <div class="videos__item-descr">
+            ${data[1][i]}
+        </div>
+        <div class="videos__item-views">
+            ${data[2][i]}
+        </div>
         `;
-
         videosWrapper.appendChild(card);
         setTimeout(() => {
-            card.classList.remove("videos__item-active");
+            card.classList.remove('videos__item-active');    
         }, 10);
+
         bindNewModal(card);
-    } 
-    if (night) {
-        drawAll('#fff');
-      }
-      sliceTitle('.videos__item-descr', 80);
-    
-}); 
+       
+    }
 
-let sliceTitle = (selector, count) => {
+    sliceTittle('.videos__item-descr', 100);
+});
+
+function sliceTittle(selector, count) {
     document.querySelectorAll(selector).forEach(item => {
-      item.textContent.trim();
+        item.textContent.trim();
 
-      if (item.textContent.length < count) {
-        return;
-      } else {
-        const str = item.textContent.slice(0, count + 1) + '...';
-        item.textContent = str;
-      }
+        if (item.textContent.length < count){
+            return;
+        }else{
+            const str = item.textContent.slice(0, count+1) + "...";
+            item.textContent = str;
+        }
     });
-  };
-  sliceTitle('.videos__item-descr', 80);
+}
+sliceTittle('.videos__item-descr', 100);
 
-  // Modal
-  let openModal = () => {
+function openModal() {
     modal.style.display = 'block';
-  };
-  let closeModal = () => {
+}
+
+function closeModal() {
     modal.style.display = 'none';
     player.stopVideo();
-  };
-    // Bind Modal for all cards
-  let bindModal = cards => {
+}
+
+function bindModal(cards) {
     cards.forEach(item => {
-      item.addEventListener('click', e => {
-        e.preventDefault();
-        const id = item.getAttribute('data-url');
-        loadVideo(id);
-        openModal();
-      });
+        item.addEventListener('click', (e) => {
+            e.preventDefault(); //отключить стандартное поведение браузера
+            const id = item.getAttribute('data-url');
+            loadVideo(id);
+            openModal();
+        })
     });
-  };
-  bindModal(videos);
-    // Bind Modal for new cards
-  let bindNewModal = card => {
-    card.addEventListener('click', e => {
-        e.preventDefault();
-        const id = card.getAttribute('data-url');
-        loadVideo(id);
+}
+
+bindModal(videos);
+
+function bindNewModal(cards) {
+    cards.addEventListener('click', (e) => {
+        e.preventDefault(); //отключить стандартное поведение браузера
+        const id = cards.getAttribute('data-url');
+            loadVideo(id);
         openModal();
-    });
-  };
-    // Overlay click close
-  modal.addEventListener('click', e => {
-    let t = e.target;
-    if(!t.classList.contains('modal__body')){
-      closeModal();
+    })
+}
+
+modal.addEventListener('click', (e) => {
+    if (!e.target.classList.contains('modal__body')) {
+        closeModal();
     }
-  });
-  document.body.addEventListener('keydown', e => {
-    if(e.which == 27){
-      closeModal();
-    }
-  });
-    // Adding videos by YouTube API
-  let createVideo = () => {
+});
+
+function createVideo() {
     var tag = document.createElement('script');
 
     tag.src = "https://www.youtube.com/iframe_api";
     var firstScriptTag = document.getElementsByTagName('script')[0];
     firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-    setTimeout(() => {
-      player = new YT.Player('frame', {
-        height: '100%',
-        width: '100%',
-        videoId: 'M7lc1UVf-VE'
-      });
-    }, 300);
-  };
 
-  
-  createVideo();
-  let loadVideo = (id) => {
-    player.loadVideoById({'videoId':`${id}`});
-  }
+    setTimeout(() => {
+        player = new YT.Player('frame', {
+            height: '100%',
+            width: '100%',
+            videoId: 'M7lc1UVf-VE'
+        });
+   
+       
+    }, 300);
+}
+
+createVideo();
+
+function loadVideo(id) {
+    player.loadVideoById({'videoId': `${id}`});
+}
